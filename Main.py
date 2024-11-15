690bbafdac5e541ea2385ce6230e5815d7fd5287
julianBogado1 <jbogado@gmail.com>
2024-11-15 10:58:24 -0300

    commented populateDb

--- Main.py
+++ Main.py
@@ -1,6 +1,6 @@
 import Persistence as p
 
 def main():
-    p.populateDb()  # This function will populate the database with the data from the csv files
-
+    #p.populateDb()  # This function will populate the database with the data from the csv files
+    pass
 main()
