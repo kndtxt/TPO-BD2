16521da693e809565a2336959cb5a69e4b92e71b
mquesada <mquesada@itba.edu.ar>
2024-11-18 17:30:55 -0300

    fix: added more dependencies for the codespace

--- run.sh
+++ run.sh
@@ -1,2 +1,5 @@
-streamlit run frontend/main.py &
-fastapi dev api/main.py
+docker run --name mongo_bd2 -p 27017:27017 -d mongo
+docker run --name redis_bd2 -p 6379:6379 -d redis
+python3 api/populate.py
+python3 -m streamlit run frontend/main.py > /dev/null &
+python3 -m fastapi dev api/main.py
